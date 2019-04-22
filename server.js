0d7abc6ba8baa3f7378979fb1275f84ce1d430d2
Alex Edwards <edwar213@purdue.edu>
2019-04-22 15:44:35 -0400

    Revert "stuff"
    This reverts commit 2d736261d2a80516f1c6d49449d99967ecbef9ec.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){
-    fs.readFile('index.html',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
-    });
-}).listen(8000);
+const PORT=8080; 
+
+fs.readFile('index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
